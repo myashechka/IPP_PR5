e00edeedc310f7f04f2fed6674a7d88a99e65109
Стрельникова Арина <arisha.strelnikova@gmail.com>
2023-12-03 20:44:32 +0300

    Change Contact.java

--- src/main/java/com/example/rschirpr5/IPP5.java
+++ src/main/java/com/example/rschirpr5/IPP5.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RschirPr5Application {
+public class IPP5 {
 
     public static void main(String[] args) {
-        SpringApplication.run(RschirPr5Application.class, args);
+        SpringApplication.run(IPP5.class, args);
     }
 
 }
